8dc63bac3d18d28e9cef481b905cfe32e361f8dc
Pavel Shaydo <zwon@trinitum.org>
2019-03-26 22:01:43 +0100

    add bindings for vi-word movement commands

--- zsh/lib/bindkeys.zsh
+++ zsh/lib/bindkeys.zsh
@@ -4,6 +4,11 @@ bindkey '\ee' emacs-forward-word
 bindkey '\ek' up-line
 bindkey '\ej' down-line
 
+bindkey '\eb' vi-backward-word
+bindkey '\ef' vi-forward-word
+bindkey '\e\C-w' vi-backward-kill-word
+bindkey '\e\C-?' vi-backward-kill-word
+
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
